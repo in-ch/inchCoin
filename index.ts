fff32e64653a510ce28d6cb9659d693ac3f6f330
inch <sxin2949@naver.com>
2021-06-22 18:46:27 +0900

    Types in Typescript

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello");
+const sayHi = (name:string, age:number, gender: string): string => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
+
+console.log(sayHi("Inch",44,"male"));
+
+export {};
+
+// void는 리턴이 없는 거고
+// string은 만그대로 문자열 반환 
