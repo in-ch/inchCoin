a02fc28422fa254f4b28de3517a64d9836d3d1b5
inch <sxin2949@naver.com>
2021-06-23 20:35:46 +0900

    #0.8 Blockchain Creating a Block

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-interface Human {
+interface Human { // js로 변환되지 않음. 
     name:string;
     age:number;
     gender:string;
@@ -9,11 +9,26 @@ const person = {
     age: 22,
     gender: "male"
 }
+
+class Human {
+    public name: string; 
+    private age2: number;
+    public gender: string; 
+    constructor(name: string, age2:number,gender?:string){
+        this.name = name;
+        this.age2 = age2;
+        this.gender = gender;
+    } // 함수인데 클래스가 호출될 때 마다 실행됨. 
+};
+
+const lynn = new Human("Lynn", 18, "female");
+
+
 const sayHi = (person: Human): string => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(lynn));
 
 export {};
 
