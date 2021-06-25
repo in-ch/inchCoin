8702d89a26834c4b89e8f6b3b82a4dd49df0d9f0
inch <sxin2949@naver.com>
2021-06-25 17:59:25 +0900

    #0.12 Validating block Structure part Two

--- blcok.ts
+++ blcok.ts
@@ -1,13 +1,16 @@
 import * as CryptoJs from "crypto-js";
 
 class Block {
+    static calculateBlockHash = (index:number, previousHash: string, timestamp:number, data: string) : string => CryptoJs.SHA256(index + previousHash + timestamp + data).toString();
+    static validateStructure = (aBlock: Block) : boolean => {
+        typeof aBlock.index === "number" && typeof aBlock.hash === "string" && typeof aBlock.previousHash === "string" && typeof aBlock.timestamp === "number" && typeof aBlock.data === "string";
+    }; 
+
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
-
-    static calculateBlockHash = (index:number, previousHash: string, timestamp:number, data: string) : string => CryptoJs.SHA256(index + previousHash + timestamp + data).toString();
 
     constructor(
         index: number,
@@ -41,6 +44,18 @@ const createNewBlock = (data:string) : Block => {
     
     return newBlock;
 };
+
+const isBlockValid = (candidateBlock : Block, previousBlock: Block) : boolean => {
+    if(Block.validateStructure(candidateBlock)) {
+        return false;
+    } else if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    } else if(previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    } else if() {
+        
+    }
+}
 
 console.log(createNewBlock("hello"), createNewBlock("byby"));
 export {};
